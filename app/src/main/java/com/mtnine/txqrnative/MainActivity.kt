00eb270606c9180f66848a73b1b4ada6d66ef937
MountainNine <goosan126@gmail.com>
2021-12-30 13:19:29 +0900

    start making qr

--- app/src/main/java/com/mtnine/txqrnative/MainActivity.kt
+++ app/src/main/java/com/mtnine/txqrnative/MainActivity.kt
@@ -2,10 +2,20 @@ package com.mtnine.txqrnative
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.mtnine.txqrnative.base.BaseActivity
+import com.mtnine.txqrnative.databinding.ActivityMainBinding
+import com.mtnine.txqrnative.vm.MainViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
+    override val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
---
+++ app/src/main/java/com/mtnine/txqrnative/vm/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.mtnine.txqrnative.vm
+
+import com.mtnine.txqrnative.base.BaseViewModel
+
+class MainViewModel : BaseViewModel() {
+    fun makeQR() {
+
+    }
+}
